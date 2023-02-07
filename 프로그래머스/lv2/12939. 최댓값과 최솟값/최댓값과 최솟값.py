c38fb1e5c165d1901932dd3854ec672296d61be6
Jisu Park <122674412+JisuPark-dev@users.noreply.github.com>
2023-02-07 11:45:28 +0900

    [level 2] Title: 최댓값과 최솟값, Time: 0.05 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/lv2/12939. 최댓값과 최솟값/최댓값과 최솟값.py
+++ 프로그래머스/lv2/12939. 최댓값과 최솟값/최댓값과 최솟값.py
@@ -1,5 +1,5 @@
 def solution(s):
-    a = list(map(str,sorted(list(map(int,s.split())))))
-    answer = ''
-    answer = answer+a[0] +' ' + a[-1]
-    return answer
+    a = sorted(list(map(int,s.split())))
+   
+    
+    return f'{a[0]} {a[-1]}'
