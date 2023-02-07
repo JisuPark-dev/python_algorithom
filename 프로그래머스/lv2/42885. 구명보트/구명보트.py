f8289f053f5549f913aad103f22b08648e7ba53c
Jisu Park <122674412+JisuPark-dev@users.noreply.github.com>
2023-02-07 21:26:30 +0900

    [level 2] Title: 구명보트, Time: 10.78 ms, Memory: 10.7 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/42885. 구명보트/구명보트.py
@@ -0,0 +1,17 @@
+from collections import deque
+def solution(people, limit):
+    answer = 0
+    people.sort()
+    people = deque(people)
+    while people:
+        now = people.pop()
+        if len(people)>=1:
+            if now + people[0]<=limit:
+                people.popleft()
+                answer+=1
+            else:
+                answer+=1
+        else:
+            answer+=1
+        
+    return answer
