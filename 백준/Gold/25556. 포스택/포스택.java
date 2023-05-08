2574f9083941a2f4c422d7c5671ae47025ea6d40
Jisu Park <122674412+JisuPark-dev@users.noreply.github.com>
2023-05-08 19:37:07 +0900

    [Gold V] Title: 포스택, Time: 628 ms, Memory: 38800 KB -BaekjoonHub

--- 백준/Gold/25556. 포스택/포스택.java
+++ 백준/Gold/25556. 포스택/포스택.java
@@ -1,23 +1,25 @@
-import java.util.Stack;
 import java.util.Scanner;
+import java.util.Stack;
 
 public class Main{
     public static void main(String[] args){
-        Scanner sc = new Scanner(System.in);
         int n;
         String str_tmp;
         String[] str = {};
+        
+        Scanner scanner = new Scanner(System.in);
+        n = Integer.parseInt(scanner.nextLine());
+        str_tmp = scanner.nextLine();
+        str = str_tmp.split(" ");
+        
+        String result = "YES";
+        
         Stack<Integer> stack1 = new Stack<>();
         Stack<Integer> stack2 = new Stack<>();
         Stack<Integer> stack3 = new Stack<>();
         Stack<Integer> stack4 = new Stack<>();
-        int result = 1;
         
-        n = Integer.parseInt(sc.nextLine());
-        str_tmp = sc.nextLine();
-        str = str_tmp.split(" ");
-        
-        for(int i =0; i< n; i++){
+        for(int i =0; i<n; i++){
             if(stack1.empty()||stack1.peek()<Integer.parseInt(str[i])){
                 stack1.push(Integer.parseInt(str[i]));
             }else if(stack2.empty()||stack2.peek()<Integer.parseInt(str[i])){
@@ -27,14 +29,9 @@ public class Main{
             }else if(stack4.empty()||stack4.peek()<Integer.parseInt(str[i])){
                 stack4.push(Integer.parseInt(str[i]));
             }else{
-                result = 0;
-            } 
+                result = "NO";
+            }
         }
-        if(result == 0){
-           System.out.println("NO");            
-        }else{
-            System.out.println("YES");
-        }
-
+        System.out.println(result);
     }
 }
