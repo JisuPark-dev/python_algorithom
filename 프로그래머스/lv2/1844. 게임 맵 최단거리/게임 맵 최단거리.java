7913b86908fc74993764145c0107ce8180a2fe19
Jisu Park <122674412+JisuPark-dev@users.noreply.github.com>
2023-05-29 12:21:11 +0900

    [level 2] Title: 게임 맵 최단거리, Time: 4.05 ms, Memory: 53 MB -BaekjoonHub

--- 프로그래머스/lv2/1844. 게임 맵 최단거리/게임 맵 최단거리.java
+++ 프로그래머스/lv2/1844. 게임 맵 최단거리/게임 맵 최단거리.java
@@ -1,24 +1,23 @@
 import java.util.*;
+
 class Solution {
     int[] dx = {1,0,-1,0};
     int[] dy = {0,1,0,-1};
     
-    public int solution(int[][] maps) {
+    public int solution(int[][] maps) {    
         int answer = 0;
         int[][] visited = new int[maps.length][maps[0].length];
-        dfs(maps, visited);
+        bfs(maps, visited);
+        
         answer = visited[maps.length-1][maps[0].length-1];
-        
-        if(answer == 0){
-            answer = -1;
+        if(answer ==0){
+            answer =-1;
         }
-            
-        
-        
         return answer;
     }
-    public void dfs(int[][] maps, int[][] visited){
-        int x = 0;
+    
+    public void bfs(int[][] maps, int[][] visited){
+        int x = 0; 
         int y = 0;
         visited[x][y] = 1;
         
@@ -34,17 +33,15 @@ class Solution {
                 int nX = cX+dx[i];
                 int nY = cY+dy[i];
                 
-                if(nX <0||nX>maps.length-1||nY<0||nY>maps[0].length-1){
+                if(nX<0||nX>maps.length-1||nY<0||nY>maps[0].length-1){
                     continue;
                 }
-                if(visited[nX][nY] == 0 && maps[nX][nY] == 1){
+                
+                if(visited[nX][nY]==0&& maps[nX][nY]==1){
                     visited[nX][nY] = visited[cX][cY]+1;
-                    queue.add(new int[]{nX,nY});
-                    
+                    queue.add(new int[]{nX, nY});
                 }
             }
-                
         }
     }
-        
 }
