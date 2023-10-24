d2828a486273cf7304b4901c291dc50341dc2fd1
Jisu Park <122674412+JisuPark-dev@users.noreply.github.com>
2023-10-24 22:47:28 +0900

    [Silver II] Title: 부분수열의 합, Time: 1220 ms, Memory: 129284 KB -BaekjoonHub

--- 백준/Silver/1182. 부분수열의 합/부분수열의 합.py
+++ 백준/Silver/1182. 부분수열의 합/부분수열의 합.py
@@ -2,23 +2,29 @@ n, m = map(int, input().split())
 inp = list(map(int, input().split()))
 arr = [0 for i in range(n)]
 answer = 0
-visited = [0 for i in range(n)]  # the size should be based on 'n' as it relates to inp[]
+visited = [0 for i in range(n)]
+
 
 def recursion(limit, index, start):
-    global answer
-    if index == limit:
-        if sum(arr[:limit]) == m:  # we should only sum up to 'limit'
-            answer += 1
-        return
+  global answer
+  if (index >= limit):
+    sum = 0
+    for i in arr:
+      sum += i
+    if (sum == m):
+      answer += 1
+  else:
     for i in range(start, len(inp)):
-        if visited[i]:
-            continue
-        visited[i] = 1
-        arr[index] = inp[i]
-        recursion(limit, index + 1, i + 1)
-        visited[i] = 0
+      if (visited[i] == 1):
+        continue
+      visited[i] = 1
+      arr[index] = inp[i]
+      recursion(limit, index + 1, i + 1)
+      visited[i] = 0
+      arr[index] = 0
+
 
 for i in range(1, n+1):
-    recursion(i, 0, 0)
+  recursion(i, 0, 0)
 
 print(answer)
